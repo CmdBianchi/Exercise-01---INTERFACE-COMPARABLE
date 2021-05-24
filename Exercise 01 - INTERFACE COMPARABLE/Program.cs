7f9fc307ee5161f8262143d88f62c5faacd75457
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-24 14:25:33 -0300

    Adicionar arquivos de projeto.

---
+++ Exercise 01 - INTERFACE COMPARABLE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Entities;
+namespace Exercise_01___INTERFACE_COMPARABLE {
+    //-----> START
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\in.txt";
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>(); 
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
